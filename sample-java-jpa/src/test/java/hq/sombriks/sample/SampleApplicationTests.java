0902d9cb73680c9fd5119a4b42efe0f658df1d7a
sombriks <sombriks@gmail.com>
2019-01-31 01:19:01 -0300

    testing if jpa works

--- sample-java-jpa/src/test/java/hq/sombriks/sample/SampleApplicationTests.java
+++ sample-java-jpa/src/test/java/hq/sombriks/sample/SampleApplicationTests.java
@@ -1,17 +1,35 @@
 package hq.sombriks.sample;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import hq.sombriks.sample.model.Person;
+
 @SpringBootTest
+@RunWith(SpringRunner.class)
 public class SampleApplicationTests {
+
+	@PersistenceContext
+	private EntityManager em;
 
 	@Test
 	public void contextLoads() {
 	}
 
-}
+	@Test
+	public void shouldListPeople() throws Exception {
+		List<Person> people = em.createQuery("select p from Person p", Person.class)//
+				.getResultList();
+		assertEquals(8, people.size());
+	}
 
+}
